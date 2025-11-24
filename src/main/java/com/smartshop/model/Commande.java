71ea7e05baed93966ebf01ad5f428fd0570c86ef
MohamedMoustir <itsmoustir@gmail.com>
2025-11-24 21:40:50 +0100

    SMAR-3-fix: Fix mapper logic & update Commande entity relationships

--- src/main/java/com/smartshop/model/Commande.java
+++ src/main/java/com/smartshop/model/Commande.java
@@ -1,43 +1,43 @@
-package com.smartshop.model;
+    package com.smartshop.model;
 
-import com.smartshop.enums.OrderStatus;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+    import com.smartshop.enums.OrderStatus;
+    import jakarta.persistence.*;
+    import lombok.AllArgsConstructor;
+    import lombok.Builder;
+    import lombok.Data;
+    import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+    import java.time.LocalDateTime;
+    import java.util.ArrayList;
+    import java.util.List;
 
-@Entity
-@Table(name = "commands")
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class Commande {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "client_id", nullable = false)
-    private Client client;
+    @Entity
+    @Table(name = "commands")
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class Commande {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "client_id", nullable = false)
+        private Client client;
 
-    private LocalDateTime date;
+        private LocalDateTime date;
 
-    @OneToMany(mappedBy = "commande", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<OrderItem> orderItems = new ArrayList<>();
+        @OneToMany(mappedBy = "commande", cascade = CascadeType.ALL, orphanRemoval = true)
+        private List<OrderItem> orderItems = new ArrayList<>();
 
-    private Double sousTotalHT;
-    private Double montantRemise;
-    private Double tva;
-    private Double totalTTC;
-    private Double montantRestant;
+        private Double sousTotalHT;
+        private Double montantRemise;
+        private Double tva;
+        private Double totalTTC;
+        private Double montantRestant;
 
-    private String codePromo;
+        private String codePromo;
 
-    @Enumerated(EnumType.STRING)
-    private OrderStatus statut;
-}
+        @Enumerated(EnumType.STRING)
+        private OrderStatus statut;
+    }
