3ec285048c0ea53053c28be7d92aead8e1b90a40
MohamedMoustir <itsmoustir@gmail.com>
2025-11-26 20:17:12 +0100

    Feat: Mise à jour de ProductRequest DTO

--- src/main/java/com/smartshop/presontation/dto/Request/ProductRequest.java
+++ src/main/java/com/smartshop/presontation/dto/Request/ProductRequest.java
@@ -1,4 +1,28 @@
 package com.smartshop.presontation.dto.Request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductRequest {
+
+    @NotBlank(message = "Le nom est obligatoire")
+    private String nom;
+
+    @NotNull(message = "Le prix est obligatoire")
+    @Min(value = 0, message = "Le prix doit être positif")
+    private Double prixUnitaire;
+
+    @NotNull(message = "Le stock est obligatoire")
+    @Min(value = 0, message = "Le stock ne peut pas être négatif")
+    private Integer stockDisponible;
+
 }
