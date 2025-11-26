9d1cf6c64033646556fe872273e68c02ee30aebd
MohamedMoustir <itsmoustir@gmail.com>
2025-11-26 20:17:28 +0100

    Feat: Ajout de OrderItemResponse DTO

--- src/main/java/com/smartshop/presontation/dto/Response/OrderItemResponse.java
+++ src/main/java/com/smartshop/presontation/dto/Response/OrderItemResponse.java
@@ -1,4 +1,19 @@
 package com.smartshop.presontation.dto.Response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderItemResponse {
+
+    private Long productId;
+    private String productName;
+    private Integer quantity;
+    private Double prixUnitaire;
+    private Double totalLigne;
 }
