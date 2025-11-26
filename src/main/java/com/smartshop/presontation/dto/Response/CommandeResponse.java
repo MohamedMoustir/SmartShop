f96b16be1c7df948b97b6608248d8b24ac6290b3
MohamedMoustir <itsmoustir@gmail.com>
2025-11-26 20:17:20 +0100

    Refactor: Renommage de CommandeDTO en CommandeResponse et mise à jour

--- src/main/java/com/smartshop/presontation/dto/Response/CommandeResponse.java
+++ src/main/java/com/smartshop/presontation/dto/Response/CommandeResponse.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class OrderResponse {
+public class CommandeResponse {
 
     private Long id;
     private Long clientId;
