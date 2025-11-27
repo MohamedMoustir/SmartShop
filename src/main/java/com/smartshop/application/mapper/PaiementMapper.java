1de3b78597680c1e28838f8d218aed19376044bc
MohamedMoustir <itsmoustir@gmail.com>
2025-11-27 17:22:03 +0100

    feat(mapper): Implement PaiementMapper and update CommandeMapper

--- src/main/java/com/smartshop/application/mapper/PaiementMapper.java
+++ src/main/java/com/smartshop/application/mapper/PaiementMapper.java
@@ -1,14 +1,29 @@
 package com.smartshop.application.mapper;
 
-import com.smartshop.presontation.dto.PaiementDTO;
 import com.smartshop.domain.model.Commande;
 import com.smartshop.domain.model.Paiement;
+import com.smartshop.presontation.dto.Request.PaiementRequest;
+import com.smartshop.presontation.dto.Response.PaiementResponse;
 
 public class PaiementMapper {
 
-    public static PaiementDTO toDto(Paiement paiement){
+    public static Paiement toEntity(PaiementRequest request){
+        if(request == null ) return null;
+        return Paiement.builder()
+                .banque(request.getBanque())
+                .commande(request.getCommandeId() != null ?
+                        Commande.builder()
+                                .id(request.getCommandeId())
+                                .build() :null )
+                .dateEcheance(request.getDateEcheance())
+                .montant(request.getMontant())
+                .typePaiement(request.getTypePaiement())
+                .reference(request.getReference())
+                .build();
+    }
+    public static PaiementResponse toResponse(Paiement paiement){
         if(paiement == null ) return null;
-        return PaiementDTO.builder()
+        return PaiementResponse.builder()
                 .id(paiement.getId())
                 .banque(paiement.getBanque())
                 .numeroPaiement(paiement.getNumeroPaiement())
@@ -19,26 +34,8 @@ public class PaiementMapper {
                 .montant(paiement.getMontant())
                 .typePaiement(paiement.getTypePaiement())
                 .reference(paiement.getReference())
+                .montantRestant(paiement.getCommande() != null ? paiement.getCommande().getMontantRestant():null)
                 .status(paiement.getStatus())
-                .build();
-    }
-    public static Paiement toEntity(PaiementDTO dto){
-        if(dto == null ) return null;
-        return Paiement.builder()
-                .id(dto.getId())
-                .banque(dto.getBanque())
-                .numeroPaiement(dto.getNumeroPaiement())
-                .commande(dto.getCommandeId() != null ?
-                        Commande.builder()
-                                .id(dto.getCommandeId())
-                        .build() :null )
-                .datePaiement(dto.getDatePaiement())
-                .dateEcheance(dto.getDateEcheance())
-                .dateEncaissement(dto.getDateEncaissement())
-                .montant(dto.getMontant())
-                .typePaiement(dto.getTypePaiement())
-                .reference(dto.getReference())
-                .status(dto.getStatus())
                 .build();
     }
 }
