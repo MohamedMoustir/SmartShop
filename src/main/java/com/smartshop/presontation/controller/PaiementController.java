de2441048c23455de8f21ec66516e349e6352544
MohamedMoustir <itsmoustir@gmail.com>
2025-11-27 17:22:23 +0100

    feat(controller): Expose Payment REST endpoints

--- src/main/java/com/smartshop/presontation/controller/PaiementController.java
+++ src/main/java/com/smartshop/presontation/controller/PaiementController.java
@@ -1,4 +1,32 @@
 package com.smartshop.presontation.controller;
 
-public class OrderController {
+import com.smartshop.application.service.PaiementServise;
+import com.smartshop.domain.enums.PaymentStatus;
+import com.smartshop.presontation.dto.Request.PaiementRequest;
+import com.smartshop.presontation.dto.Response.PaiementResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/paiements")
+@RequiredArgsConstructor
+public class PaiementController {
+
+    private final PaiementServise paiementServise;
+    @PostMapping
+    public ResponseEntity<PaiementResponse> creerPaiement(@Valid @RequestBody PaiementRequest request) {
+        PaiementResponse response = paiementServise.enregistrerPaiement(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PutMapping("{id}/status")
+    public ResponseEntity<PaiementResponse> mettreAJourStatutPaiement(
+            @Valid @PathVariable("id") Long id
+            ,  @RequestParam("nouveauStatut") PaymentStatus nouveauStatut ) {
+        PaiementResponse response = paiementServise.mettreAJourStatutPaiement(id ,nouveauStatut);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
