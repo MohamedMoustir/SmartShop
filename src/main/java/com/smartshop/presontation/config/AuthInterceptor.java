5ca13d59fb71166ef786729664669c0fb5e522b8
MohamedMoustir <itsmoustir@gmail.com>
2025-12-01 18:04:02 +0100

    feat(auth): refine AuthInterceptor rules

--- src/main/java/com/smartshop/presontation/config/AuthInterceptor.java
+++ src/main/java/com/smartshop/presontation/config/AuthInterceptor.java
@@ -8,15 +8,17 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import static com.smartshop.presontation.controller.AuthController.USER_ID_KEY;
+import static com.smartshop.presontation.controller.AuthController.USER_ROLE_KEY;
+
 @Component
 public class AuthInterceptor implements HandlerInterceptor {
-    public static final String USER_ID_KEY = "USER_ID";
-    public static final String USER_ROLE_KEY = "USER_ROLE";
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-        HttpSession session =request.getSession(false);
+        HttpSession session = request.getSession(false);
+
         if(session == null || session.getAttribute(USER_ID_KEY) == null){
             throw new UnauthorizedException("Session expirée ou non authentifié. Veuillez vous connecter.");
         }
@@ -24,11 +26,11 @@ public class AuthInterceptor implements HandlerInterceptor {
         String userRle = session.getAttribute(USER_ROLE_KEY).toString();
         String requestPath = request.getRequestURI();
 
-        if(requestPath.startsWith("/api/clients") || requestPath.startsWith("/api/products") || requestPath.startsWith("/api/commandes")){
+        if(requestPath.startsWith("/api/clients") || requestPath.startsWith("/api/products") || requestPath.startsWith("/api/commandes") || requestPath.startsWith("/api/paiements")){
             if("CLIENT".equals(userRle) && !request.getMethod().equalsIgnoreCase("GET")){
                 throw new ForbiddenException("Accès refusé. Seul l'ADMIN peut créer, modifier ou supprimer ces ressources.");
             }
-            if("CLIENT".equals(userRle) && requestPath.startsWith("/api/client")&& request.getMethod().equalsIgnoreCase("GET")){
+            if("CLIENT".equals(userRle) && requestPath.startsWith("/api/clients")&& request.getMethod().equalsIgnoreCase("GET")){
                 throw new ForbiddenException("Accès refusé. Un CLIENT ne peut pas voir la liste des autres clients.");           }
 
         }
